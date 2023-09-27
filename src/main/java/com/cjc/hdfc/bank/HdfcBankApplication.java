001d679c06ff967c2988343fbdc2ab00e92b2d96
sajed013 <84460064+sajed013@users.noreply.github.com>
2023-09-27 11:12:25 +0530

    hdfc saveing account

---
+++ src/main/java/com/cjc/hdfc/bank/HdfcBankApplication.java
@@ -0,0 +1,13 @@
+package com.cjc.hdfc.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdfcBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdfcBankApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cjc/hdfc/bank/controller/Hdfc_Controller.java
@@ -0,0 +1,15 @@
+package com.cjc.hdfc.bank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hdfc_Controller {
+
+	@GetMapping("/getinfo")
+	public String Getinformation() {
+
+		return "This is Hdgfc bank ,Welcome to ";
+	}
+
+}
